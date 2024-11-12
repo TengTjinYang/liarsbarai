499a37edd6e2b6961b18525d69af5ed548c71a65
TengTjinYang <101085998+TengTjinYang@users.noreply.github.com>
2024-11-12 15:49:28 +0800

    Initial commit

---
+++ lmao.py
@@ -0,0 +1,9 @@
+# import random
+
+# for _ in range(6):
+#     revolvers = []
+#     chamber = [0] * 6
+#     chamber[random.randint(0, 5)] = 1  # Place bullet randomly
+#     revolvers.append(chamber)
+
+# print(revolvers)
